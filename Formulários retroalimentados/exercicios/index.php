ff7f1e1bcbf2730c73bc1b5c4240466d79d98365
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-06-13 09:40:36 -0300

    formularios retroalimentados

---
+++ Formulários retroalimentados/exercicios/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <form action="" method="get">
+            <label for="v1">Valor 1</label>
+            <input type="number" name="v1" id="v1">
+            <label for="v2">Valor 2</label>
+            <input type="number" name="v2" id="v2">
+            <input type="submit" value="Somar">
+        </form>
+    </main>
+</body>
+</html>
